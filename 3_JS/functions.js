4c682c90f6e1bfd5d989365da027cfb7d48be9f7
wakekarsanket <sanketwakekar@outlook.com>
2022-06-19 13:27:49 +0530

    🚧 learning JS

--- 3_JS/functions.js
+++ 3_JS/functions.js
@@ -1,4 +1,4 @@
-// 1) Normal Function 
+// 1) Normal Function
 
 //function definition
 // function nameOfTheFunction(param1, param2) {
@@ -10,32 +10,30 @@
 
 //write a function to add 2 numbers
 function add(num1, num2) {
-    var ans = num1 + num2
-    // console.log(ans);
-    return ans;
+  var ans = num1 + num2;
+  // console.log(ans);
+  return ans;
 }
 
 let res = add(3, 4);
 console.log(res);
 
-//functions are treated as first class citizens in JS 
+//functions are treated as first class citizens in JS
 // -> functions can be returned
 // -> functions can be passed as parameters/args
 
 // build a calculator
 function calculator(operator, num1, num2) {
-    if (operator == "add") {
-        return function add() {
-            // console.log(num1+num2);
-            return num1 + num2;
-        }
-    }
-
-    else if (operator == "-") {
-        return function sub() {
-            console.log(num1-num2);
-        }
-    }
+  if (operator == "add") {
+    return function add() {
+      // console.log(num1+num2);
+      return num1 + num2;
+    };
+  } else if (operator == "-") {
+    return function sub() {
+      console.log(num1 - num2);
+    };
+  }
 }
 
 let retFn = calculator("add", 45, 46);
@@ -43,19 +41,17 @@ console.log(retFn);
 let sumOfTwoNum = retFn();
 console.log(sumOfTwoNum);
 
-
-
 // 2) function expression
 
 var variable_name = function (params) {
-    //do something
-}
+  //do something
+};
 
 variable_name();
 
 var sayHi = function () {
-    console.log("hello how r u ?");
-}
+  console.log("hello how r u ?");
+};
 
 sayHi();
 console.log(sayHi);
@@ -63,15 +59,15 @@ console.log(sayHi);
 // 3) IIFE -> immediately invoked function
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 let ans = add(2, 3);
 console.log(ans);
 
 let additionIIFE = (function (a, b) {
-    // console.log(a + b);
-    return a + b;
+  // console.log(a + b);
+  return a + b;
 })(20, 30);
 
 console.log(additionIIFE);
