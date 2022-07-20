41fd58b8a30d64e377b4f9aabaa8a712f69776c4
wakekarsanket <sanketwakekar@outlook.com>
2022-07-20 22:57:18 +0530

    🔥 map,filter,reduce interview ques

--- 3_JS/hof.js
+++ 3_JS/hof.js
@@ -134,3 +134,122 @@ function greaterThan4(num) {
 }
 var ans = arr.filter(greaterThan4);
 console.log(ans);
+
+//reduce
+
+// var arr = [2, 5, 7, 12];
+
+// function sumOfArr(acc, cur) {
+//   console.log("Before");
+//   console.log(acc); //2 
+//   console.log(cur); //5 
+//   acc = acc + cur;
+//   console.log("After");
+//   console.log(acc); //7 
+//   return acc;
+// }
+
+// let ans = arr.reduce(sumOfArr);
+// console.log(ans);
+
+
+// var arr = [10, -5, 566, 28, 56, 12,456];
+
+// function largestValue(acc, cur) {
+//   console.log("Before");
+//   console.log(acc);
+//   console.log(cur);
+  
+//   if (cur > acc) {
+//     acc = cur;
+//   }
+
+//   console.log("After");
+//   console.log(acc);
+//   return acc;
+// }
+//                           //cb , initial value of acc, so that cur starts with 0th index  
+// let ans = arr.reduce(largestValue, -Infinity);
+
+// console.log(ans);
+
+const worldCapitals = [
+  {
+    country: "India",
+    capital: "Delhi",
+    population: "2cr",
+  },
+  {
+    country: "America",
+    capital: "Washington DC",
+    population: "6cr",
+  },
+  {
+    country: "France",
+    capital: "Paris",
+    population: "2cr",
+  },
+
+  {
+    country: "England",
+    capital: "London",
+    population: "4cr",
+  },
+  {
+    country: "Germany",
+    capital: "Berlin",
+    population: "2cr",
+  },
+];
+
+//Q1 print country name and their capital  
+// [ India->Delhi, America->WashingtonDC, France->Paris , England->London, Germany->Berlin] 
+
+function getCountryCapital(obj) {
+  return obj.country + "->" + obj.capital;
+}
+
+var res = worldCapitals.map(getCountryCapital);
+console.log(res);
+
+//Q2-> Return the number of countries with a particular population 
+//{ 2cr: 3, 4cr: 1, 6cr: 1 }
+
+function getCountryPopulation(obj, cur) {
+  //obj["2cr"]
+  if (obj[cur.population]) {
+    obj[cur.population] = obj[cur.population] + 1;
+  } else {
+    obj[cur.population] = 1; //{ "2cr":2,"6cr":1}
+  }
+  return obj;
+}
+
+var res = worldCapitals.reduce(getCountryCapital,{});
+console.log(res);
+
+//Q3 get country name with population less than 5cr
+//[India, France,England,Germany]
+
+function getPopulationLessThan5cr(obj) {
+  if (obj.population < "5cr") {
+    return true;
+  }
+}
+function getCountry(obj) {
+  return obj.country;
+}
+var res = worldCapitals.filter(getPopulationLessThan5cr).map(getCountry);
+console.log(res);
+
+//using reduce 
+
+function getPopulationLessThan5cr(arr, obj) {
+  if (obj.population < "5cr") {
+    arr.push(obj.country);
+  }
+  return arr;
+}
+
+var ans = worldCapitals.reduce(getPopulationLessThan5cr,[]);
+console.log(ans);
